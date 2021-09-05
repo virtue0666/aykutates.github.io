c9b4d79dc49a9cad0534d1d57a412a846baf01a4
aykutates <90139226+aykutates@users.noreply.github.com>
2021-09-05 15:42:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Frontend Developer"],
+        strings: ["Ateş"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
